832684638683dccb5041a0b7e8eb3d2861d9a23a
d.o <osip19@mail.ru>
2023-02-16 17:30:21 +0300

    work

--- seminar1/task3-1/Program.cs
+++ seminar1/task3-1/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("Введите число: ");
 string? dayString = Console.ReadLine();
 int numberDay = Convert.ToInt32(dayString);
+if (numberDay > 7) Console.WriteLine("нет такого дня недели");
 string[] days  =   new string [] {"Понедельник" , "Вторник",  "Среда" , "Четверг" , "Пятница" , "Суббота" , "Воскресенье"};
 Console.WriteLine (days[numberDay - 1]) ;
-if (dayString )
