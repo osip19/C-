080419c703f78879981d5768698450312f96fa6a
d.o <osip19@mail.ru>
2023-02-17 10:17:29 +0300

    внес ряд изменений

--- example/Program.cs
+++ example/Program.cs
@@ -4,18 +4,18 @@
 
 int xa = 40, ya = 1;
 int xb = 1, yb = 30;
-int xc = 80, yc = 30;
+int xc = 60, yc = 30;
 
-Console.SetCursorPosition(xa, ya);
-System.Console.WriteLine("*");
-Console.SetCursorPosition(xb, yb);
-System.Console.WriteLine("*");
-Console.SetCursorPosition(xc, yc);
-System.Console.WriteLine("*");
+// Console.SetCursorPosition(xa, ya);
+// System.Console.WriteLine("*");
+// Console.SetCursorPosition(xb, yb);
+// System.Console.WriteLine("*");
+// Console.SetCursorPosition(xc, yc);
+// System.Console.WriteLine("*");
 
 int x = xa, y = xb;
 int count = 0;
-while (count < 10)
+while (count < 10000)
 {
 	int what = new Random().Next(0, 3);
 	if (what == 0)
@@ -33,7 +33,9 @@ while (count < 10)
 		x = x + xc / 2;
 		y = y + yc / 2;
 	}
-	Console.SetCursorPosition(x, y);
-	System.Console.WriteLine("*");
-	count = count++;
+
 }
+
+Console.SetCursorPosition(x, y);
+System.Console.WriteLine("+");
+count = count++;
