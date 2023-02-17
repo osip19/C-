ff914127b018ad7dd2b80ddb81d04a14c3ff0154
d.o <osip19@mail.ru>
2023-02-17 08:33:02 +0300

    we continue

---
+++ example/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// System.Console.WriteLine("*");
+
+int xa = 40,  ya = 1;
+int xb = 1,  yb = 30;
+int xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+System.Console.WriteLine("*");
+Console.SetCursorPosition(xb, yb);
+System.Console.WriteLine("*");
+Console.SetCursorPosition(xc, yc);
+System.Console.WriteLine("*");
--- seminar1/task3-1/Program.cs
+++ seminar1/task3-1/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите число: ");
 string? dayString = Console.ReadLine();
 int numberDay = Convert.ToInt32(dayString);
-if (numberDay < 7)
+if (numberDay <= 7 && numberDay >=1)
 {
 	string[] days = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
 	Console.WriteLine(days[numberDay - 1]);
