dd6526e7ddd5c623bce12aae9c6767f8c5b66753
d.o <osip19@mail.ru>
2023-02-17 13:22:49 +0300

    поправил баг, можно работать сразу с двумя приветс

--- example1/Program.cs
+++ example1/Program.cs
@@ -1,14 +1,16 @@
-﻿// System.Console.Write("Введите Ваше имя: ");
-// string? userName = System.Console.ReadLine();
-// System.Console.Write("Привет, " + userName);
+﻿System.Console.Write("Введите Ваше имя: ");
+string? userName = System.Console.ReadLine();
+System.Console.WriteLine("Привет, " + userName);
+
+System.Console.WriteLine("Начало следующего приветствия");
 
 System.Console.Write("Введите Ваше имя: ");
-string? userName = System.Console.ReadLine();
-if (userName?.ToLower() == "маша")
+string? user1Name = System.Console.ReadLine();
+if (user1Name?.ToLower() == "маша")
 {
 	System.Console.Write("Ура, это же Маша!");
 }
 else {
-	System.Console.WriteLine("Привет, " + userName);
+	System.Console.WriteLine("Привет, " + user1Name);
 }
 
