5fc9a29fd6a37adebc643cc5f280ccf699c1ace2
d.o <osip19@mail.ru>
2023-02-19 21:51:37 +0300

    просто коммит

--- homeWork/homeWork10/Program.cs
+++ homeWork/homeWork10/Program.cs
@@ -4,15 +4,16 @@
 // 32679 -> 6
 
 
-System.Console.WriteLine("Введите число: ");
-int a = Convert.ToInt32(System.Console.ReadLine());
-if (a <= 99)
-	System.Console.WriteLine("В указанном числе нет третьей цифры");
-else (a <= 999)
-{ System.Console.WriteLine(a % 10); }
-// for (int i = 0; i >= 999; i++)
-// 	else if (a / 10 >= 999) ;
-// System.Console.WriteLine(a % 10);
+// System.Console.WriteLine("Введите число: ");
+// int a = Convert.ToInt32(System.Console.ReadLine());
+// if (a <= 99)
+// 	System.Console.WriteLine("В указанном числе нет третьей цифры");
 
+// else if (a > 1000) a = a / 10;
+// else if (a <= 999 && a >= 100)
+// { System.Console.WriteLine(a % 10); }
 
-// System.Console.WriteLine(a);
+int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n) - 2;
+Console.WriteLine(k < 0 ? "No such digit" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+// Console.ReadLine();
