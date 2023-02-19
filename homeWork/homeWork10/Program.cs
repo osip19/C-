0a18e0b863b3d28538d91f936813e3e9078e5d75
d.o <osip19@mail.ru>
2023-02-20 00:29:28 +0300

    ничего не понял(

--- homeWork/homeWork10/Program.cs
+++ homeWork/homeWork10/Program.cs
@@ -3,17 +3,34 @@
 // 78->третьей цифры нет
 // 32679 -> 6
 
-
-// System.Console.WriteLine("Введите число: ");
-// int a = Convert.ToInt32(System.Console.ReadLine());
-// if (a <= 99)
-// 	System.Console.WriteLine("В указанном числе нет третьей цифры");
-
-// else if (a > 1000) a = a / 10;
-// else if (a <= 999 && a >= 100)
-// { System.Console.WriteLine(a % 10); }
-
-int n = int.Parse(Console.ReadLine());
-int k = (int)Math.Log10(n) - 2;
-Console.WriteLine(k < 0 ? "No such digit" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+// 																					РЕШЕНИЕ ИЗ ИНТЕРНЕТА
+// int n = int.Parse(Console.ReadLine());
+// int k = (int)Math.Log10(n) - 2;
+// Console.WriteLine(k < 0 ? "No such digit" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
 // Console.ReadLine();
+// 
+// 																								И ЕЩЕ ОДНО
+// int n = int.Parse(Console.ReadLine()!); ;
+// 	int d = GetSecondDigit(n);
+// 	Console.WriteLine(d);
+// 	// Console.ReadKey();
+// static int GetSecondDigit(int k)
+// {
+// 	while (k >= 1000) k /= 10;
+// 	int d = k % 10;
+// 	return d;
+// }
+
+System.Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(System.Console.ReadLine());
+if (a <= 99)
+	System.Console.WriteLine("В указанном числе нет третьей цифры");
+
+while (a >= 1000) a /= 10;
+int b = a % 10;
+if (b < 1000)
+	System.Console.WriteLine(b);
+else
+{
+	System.Console.WriteLine("Непредвиденная ошибка");
+}
